1a6f43db4020f0d4c2f10c4b3f6f3a670b2656c5
stigmatta <morrigun0@gmail.com>
2025-04-24 01:26:41 +0300

    unit of work

---
+++ Data-Access/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using Data_Access.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Access.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        UserRepository UserRepository { get; }
+        GameRepository GameRepository { get; }
+        WishlistRepository WishlistRepository { get; }
+        GameGalleryRepository GameGalleryRepository { get; }
+        UserGamesRepository UserGameRepository { get; }
+        UserAchievementRepository UserAchievementRepository { get; }
+        FriendRepository FriendRepository { get; }
+        ReviewRepository ReviewRepository { get; }
+        BlockedUserRepository BlockedUserRepository { get; }
+        NewsRepository NewsRepository { get; }
+        MinimumRequirementRepository MinimumRequirementRepository { get; }
+        RecommendedRequirementRepository RecommendedRequirementRepository { get; }
+        Task Save();
+    }
+}
---
+++ Data-Access/Repositories/UnitOfWork.cs
@@ -0,0 +1,117 @@
+﻿using Data_Access.Context;
+using Data_Access.Interfaces;
+
+namespace Data_Access.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly StoreContext _context;
+        private UserRepository _userRepository;
+        private GameRepository _gameRepository;
+        private WishlistRepository _wishlistRepository;
+        private GameGalleryRepository _gameGalleryRepository;
+        private UserGamesRepository _userGameRepository;
+        private UserAchievementRepository _userAchievementRepository;
+        private FriendRepository _friendRepository;
+        private ReviewRepository _reviewRepository;
+        private BlockedUserRepository _blockedUserRepository;
+        private NewsRepository _newsRepository;
+        private MinimumRequirementRepository _minimumRequirementRepository;
+        private RecommendedRequirementRepository _recommendedRequirementRepository;
+
+        public UnitOfWork(StoreContext context)
+        {
+            _context = context;
+        }
+        public UserRepository UserRepository {
+            get
+            {
+                return _userRepository??new UserRepository(_context);
+            }
+        }
+
+        public GameRepository GameRepository
+        {
+            get
+            {
+                return _gameRepository ?? new GameRepository(_context);
+            }
+        }
+        }
+
+        public WishlistRepository WishlistRepository {
+            get
+            {
+                return _wishlistRepository ?? new WishlistRepository(_context);
+            }
+        };
+
+        public GameGalleryRepository GameGalleryRepository {
+            get
+            {
+                return _gameGalleryRepository ?? new GameGalleryRepository(_context);
+            }
+        };
+
+        public UserGamesRepository UserGameRepository {
+            get
+            {
+                return _userGameRepository ?? new UserGamesRepository(_context);
+            }
+        };
+
+        public UserAchievementRepository UserAchievementRepository {
+            get
+            {
+                return _userAchievementRepository ?? new UserAchievementRepository(_context);
+            }
+        };
+
+        public FriendRepository FriendRepository {
+            get
+            {
+                return _friendRepository ?? new FriendRepository(_context);
+            }
+        };
+
+        public ReviewRepository ReviewRepository {
+            get
+            {
+                return _reviewRepository ?? new ReviewRepository(_context);
+            }
+        };
+
+        public BlockedUserRepository BlockedUserRepository {
+            get
+            {
+                return _blockedUserRepository ?? new BlockedUserRepository(_context);
+            }
+        };
+
+        public NewsRepository NewsRepository {
+            get
+            {
+                return _newsRepository ?? new NewsRepository(_context);
+            }
+        };
+
+        public MinimumRequirementRepository MinimumRequirementRepository {
+            get
+            {
+                return _minimumRequirementRepository ?? new MinimumRequirementRepository(_context);
+            }
+        };
+
+        public RecommendedRequirementRepository RecommendedRequirementRepository {
+            get
+            {
+                return _recommendedRequirementRepository ?? new RecommendedRequirementRepository(_context);
+            }
+        }
+
+        public async Task Save()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
