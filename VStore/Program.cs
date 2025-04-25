ca70f061023e22806fbc875b2dc6f144aa6d9570
stigmatta <morrigun0@gmail.com>
2025-04-25 13:58:37 +0300

    unit of work as service

--- VStore/Program.cs
+++ VStore/Program.cs
@@ -2,25 +2,21 @@ using Business_Logic.Mappers.UserProfile;
 using Business_Logic.Services;
 using Data_Access.Context;
 using Data_Access.Interfaces;
-using Data_Access.Models;
 using Data_Access.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.FileProviders;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Добавляем сервисы
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddAuthorization();
 builder.Services.AddControllers();
 
-// Настройка БД
 string? connection = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<StoreContext>(options =>
     options.UseSqlServer(connection, b => b.MigrationsAssembly("Data-Access")));
 
-// Настройка CORS
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowReact",
@@ -30,19 +26,12 @@ builder.Services.AddCors(options =>
 });
 builder.Services.AddAutoMapper(typeof(RegistrationProfile).Assembly);
 
-// Регистрация репозиториев
-builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-builder.Services.AddScoped<IListRepository<Game>, GameRepository>();
-builder.Services.AddScoped<IListRepository<RecommendedRequirement>, RecommendedRequirementRepository>();
-builder.Services.AddScoped<IListRepository<MinimumRequirement>, MinimumRequirementRepository>();
-builder.Services.AddScoped<IRepository<GameGallery>, GameGalleryRepository>();
 
-//Сервисы
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddTransient<UserService>();
 
 var app = builder.Build();
 
-// Конфигурация middleware
 app.UseCors("AllowReact");
 
 if (app.Environment.IsDevelopment())
@@ -53,7 +42,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-// ВАЖНО: StaticFiles ДОЛЖЕН быть перед UseAuthorization и MapControllers
 app.UseStaticFiles();
 app.UseStaticFiles(new StaticFileOptions
 {
@@ -65,7 +53,6 @@ app.UseStaticFiles(new StaticFileOptions
 app.UseAuthorization();
 app.MapControllers();
 
-// Создаем папку uploads при запуске
 var uploadsPath = Path.Combine(builder.Environment.WebRootPath, "uploads");
 if (!Directory.Exists(uploadsPath))
 {
